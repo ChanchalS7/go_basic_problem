159e208588222d2110f8d2df8b56102cac5797b1
ChanchalS7 <chsverma7@gmail.com>
2024-01-01 21:25:53 +0530

    add golang basic program

---
+++ 02-Variables-Datatypes/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables
+	var i int
+	var s string
+
+	// Initializing Variables
+	i = 20
+	s = "Some String"
+
+	fmt.Println(i)
+	fmt.Println(s)
+	var k int = 32
+
+	fmt.Println(k)
+	j := 30
+	fmt.Println(j)
+
+	firstName, lastName := "Fistname", "LastName"
+	fmt.Println(firstName, lastName)
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 5
+	fmt.Println(i)
+	var s string = "Helllo World"
+	fmt.Println(s)
+	var f float64 = 3.14
+	fmt.Println
+}
