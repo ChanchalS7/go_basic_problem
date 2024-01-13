4ab1ee422f4e27e4c0b3dbb6e1a1d4a02b028653
ChanchalS7 <chsverma7@gmail.com>
2024-01-14 00:34:07 +0530

    change values of elements in slice

--- 08-Slices/Slices.go
+++ 08-Slices/Slices.go
@@ -14,4 +14,32 @@ func main() {
 	//creating slice using make function
 	var y = make([]string, 10, 20)
 	fmt.Printf("y \tLen: %v \tCap:%v\n", len(y), cap(y))
+
+	//initialize the slices with values using slice literal
+	var z = []int{10, 20, 30, 40}
+	fmt.Printf("z \tLen: %v \tCap: %v\n", len(z), cap(z))
+	fmt.Println(z)
+
+	//creating a slice using the new keyword
+	var a = new([50]int)[2:10]
+	fmt.Printf("a \tLen:%v \tCap: %v\n", len(a), cap(a))
+	fmt.Println(a)
+
+	//add items using append function
+	var b = make([]int, 1, 100)
+	fmt.Println("b:", b)
+	b = append(b, 20)
+	fmt.Println(b)
+
+	//access slice items
+	var c = []int{10, 20, 30, 40}
+	fmt.Println("C", c[0])
+	fmt.Println(c[0:3])
+
+	//change items values
+	var d = []int{10, 20, 30, 40}
+	fmt.Println("D:", d)
+	d[1] = 35
+	fmt.Println(d)
+
 }
