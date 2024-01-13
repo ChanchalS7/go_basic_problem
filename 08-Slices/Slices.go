d9f28827bd36ef4c430e14179f37e87df2a9f82a
ChanchalS7 <chsverma7@gmail.com>
2024-01-14 00:08:06 +0530

    create slice

--- 08-Slices/Slices.go
+++ 08-Slices/Slices.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	//create empty slices
+	var x []int
 
+	fmt.Println(reflect.ValueOf(x).Kind())
+
+	//creating slice using make function
+	var y = make([]string, 10, 20)
+	fmt.Printf("y \tLen: %v \tCap:%v\n", len(y), cap(y))
 }
